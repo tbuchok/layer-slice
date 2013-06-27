00b1d8c2e75fe9295682f13f7b844a4cca0f60a1
Tom Buchok <git@buchok.com>
2013-06-27 16:48:00 -0400

    refactors commonjsx to handle node modules dependencies

--- common.jsx
+++ common.jsx
@@ -5,53 +5,61 @@
 
   var LOCAL_PATHS = ['node_modules']
     , FILE_TYPES = [ '.jsx', '.js', '.json' ]
-    , dependencies = {};
+    , path = '~/Documents/Hacker-School/layer-slice/'
+    , pathStack = [];
 
   var findModule = function(moduleName) { 
-    return (dependencies[moduleName]) ? findModuleInPath(dependencies[moduleName]) : null;
+    var dir = path + 'node_modules/' + pathStack.join('/node_modules/');
+    var pkgFile = new File(dir + '/package.json');
+    pkgFile.open('r');
+    var pkg = JSON.parse(pkgFile.read());
+    var mainFile = dir + '/' + pkg.main;
+    var result = findModuleInPath(mainFile); 
+    return result;
   }
 
   var findModuleInPath = function(pathAndModuleName) {
     var file;
+    // See if the file was referenced with an extension in `require`:
+    file = new File(pathAndModuleName);
+    if (file.exists) { return file; };
+
+    // Try for designated file types if not appended with an extension:
     for (var i = 0; i < FILE_TYPES.length; i++) {
       file = new File(pathAndModuleName + FILE_TYPES[i]);
-      if (file.exists) break;
+      if (file.exists) { break; };
     }
+
     return (file.exists) ? file : null;
   }
 
   var require = function(moduleName) {
+
+    // TODO: check if the module is already loaded!!
+
     var module = { exports : {} }
       , exports = module.exports
-      , path = '~/Documents/Hacker-School/layer-slice/'
+      , pkg = false
       , file;
 
     if (/^\./.test(moduleName)) {
       file = findModuleInPath(path + moduleName);
     } else {
+      pathStack.push(moduleName);
+      pkg = true;
       file = findModule(moduleName);
     }
     
     if (!file) { throw new Error('Cannot find ' + moduleName); };
-    
+
     $.evalFile(file);
+    
+    if (pkg) { pathStack.pop(); }
+
     return exports;
   };
 
-  var pkgFile = new File('~/Documents/Hacker-School/layer-slice/package.json');
-  pkgFile.open('r');
-  var pkg = JSON.parse(pkgFile.read());
-  var pkgDependencies = pkg.dependencies;
-  for (var i in pkgDependencies) {
-    // Look into this directory and grab main.js -- add to map
-    // Look into the new dependencies and add those dependencies too!
-    dependencies[i] = '~/Documents/Hacker-School/layer-slice/node_modules/' + i + '/' + i;
-  }
-
-  // Export #require
   global.require = require; 
-
-  // Export "stdlib" functions:
   global.console = require('./stdlib/console');
 
 }(this));
